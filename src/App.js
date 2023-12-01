6dc0d0f7212f8eaccf52d4dbbbc34a5a38837a3d
Carlos Meneses <cmeneses@espol.edu.ec>
2023-11-30 20:37:34 -0500

    Admin

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import SignupPage from './pages/Signup';
 import LoginPage from './pages/Login';
 import HomePage from './pages/Home';
 
+import AdminPrincipalPage from "./pages/AdminPrincipal";
 function App() {
   return (
     <div className="min-h-full h-screen flex justify-center bg-stone-200">
@@ -17,6 +18,9 @@ function App() {
             <Route path="/" element={<LoginPage />} />
             <Route path="signup" element={<SignupPage />} />
             <Route path="home" element={<HomePage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/admin" element={<AdminPrincipalPage />} />
+            <Route path="/usuarios_admin" element={<AdminPrincipalPage />} />
           </Routes>
         </BrowserRouter>
       </div>
