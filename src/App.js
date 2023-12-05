16c56f298150dd47bc948cab6bb2557753a83f7b
koalaspero <diedarte@espol.edu.ec>
2023-12-05 10:25:11 -0500

    Home

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import SignupPage from './pages/Signup';
 import LoginPage from './pages/Login';
+import HomePage from './pages/Home';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Routes>
             <Route path="/" element={<LoginPage />} />
             <Route path="signup" element={<SignupPage />} />
+            <Route path="home" element={<HomePage />} />
           </Routes>
         </BrowserRouter>
       </div>
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import Header from "../components/Header"
+
+
+export default function HomePage(){
+    return(
+        <>
+             <Header heading="Centro de Análisis de Escritura"/>
+        </>
+    )
+}
