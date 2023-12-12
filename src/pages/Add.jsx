4f053d7974ed95412e5ab0e124e08487ce824668
Carlos Meneses <cmeneses@espol.edu.ec>
2023-12-12 18:54:11 -0500

    Carlos:Registros

--- src/pages/Add.jsx
+++ src/pages/Add.jsx
@@ -1,12 +1,26 @@
+import React, { useState } from "react";
+import UserRegistrationModal from "./UserRegistrationModal";
 const AddUser = ({ data = [], fileName }) => {
+  const [showModal, setShowModal] = useState(false);
+   const handleShowModal = () => {
+    setShowModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  };
   return (
-    <button
-      className="add-btn ml-auto"
-      onClick={() => {
-      }}
-    >
-      Registrar Usuarios
-    </button>
+    <>
+      <button
+        className="add-btn ml-auto"
+        onClick={() => {
+          handleShowModal();
+        }}
+      >
+        Registrar Usuarios
+      </button>
+      <UserRegistrationModal show={showModal} onClose={handleCloseModal} />
+    </>
   );
 };
 
