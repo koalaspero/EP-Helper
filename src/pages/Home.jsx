6b84d97920d9e3b61e62cafc4760656bc5ca4c25
koalaspero <diedarte@espol.edu.ec>
2023-12-14 10:07:50 -0500

    Home a falta de cookies

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Home from "../components/Home"
 export default function HomePage(){
     return(
         <>
-             <Header heading="Centro de Análisis de Escritura"/>
+            <Header heading="Centro de Análisis de Escritura" isHome = {true} />
              <Home />
         </>
     )
